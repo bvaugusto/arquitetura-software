df8e5118f9f76235c6ba02af17c7ebd0bb04ff78
Bruno Augusto <bvaugusto@Brunos-Mac-mini.local>
2019-04-11 09:08:31 -0300

    Implementando os métodos da classe Book

--- aula02/Controllers/BookController.cs
+++ aula02/Controllers/BookController.cs
@@ -6,26 +6,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Aula02.Controllers
 {
-    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class BookController : ControllerBase
     {
         // GET api/book
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<List<Book>> Get()
         {
             try
             {
-                return new string[] { "value1", "value2" };
+
+                List<Book> result = new List<Book>();
+                result.Add(new Book(1, "Teste 1"));
+                result.Add(new Book(2, "Teste 2"));
+                result.Add(new Book(3, "Teste 3"));
+                result.Add(new Book(4, "Teste 4"));
+                result.Add(new Book(5, "Teste 5"));
+                result.Add(new Book(6, "Teste 6"));
+                result.Add(new Book(7, "Teste 7"));
+
+                return result;
             }
             catch (System.Exception)
             {
-                
                 throw;
             }
-            
+
         }
+
 
         // GET api/book/5
         [HttpGet("{id}")]
@@ -87,5 +96,19 @@ namespace Aula02.Controllers
                 throw;
             }
         }
+    }
+
+    public class Book
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+
+        public Book(int id, string nome)
+        {
+            this.id = id;
+            this.nome = nome;
+        }
+
+
     }
 }
