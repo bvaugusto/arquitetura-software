0f29f25ceb62935f9a143864a84cf83263876e8d
Bruno Vasconcellos Augusto <478786@sga.pucminas.br>
2019-08-01 21:27:40 -0300

    DDD.Domain Interfaces

--- analise-projeto-avaliacao-arquiteturas/DDD/DDD.Domain/Interfaces/IRepository.cs
+++ analise-projeto-avaliacao-arquiteturas/DDD/DDD.Domain/Interfaces/IRepository.cs
@@ -1,7 +1,19 @@
+using DDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
 namespace DDD.Domain.Interfaces
 {
-    public class IRepository
+    public interface IRepository<T> where T : BaseEntity
     {
-        
+        void Insert(T obj);
+
+        void Update(T obj);
+
+        void Delete(int id);
+
+        T Select(int id);
+
+        IList<T> Select();
     }
 }
