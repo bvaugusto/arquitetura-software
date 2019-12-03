350d77fc76e89f6c563fef709a003ab92fa49718
Bruno <bvaugusto@gmail.com>
2019-12-03 09:38:51 -0300

    Projeto inicial Arquitetura para Aplicações Móveis

---
+++ arquitetura-front-end/otimizacao-sites/site-otimizado/src/main.js
@@ -0,0 +1,18 @@
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'font-awesome/css/font-awesome.css';
+
+import  'jquery/dist/jquery.js';
+// import 'bootstrap/dist/js/bootstrap.js';
+
+
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
+import './assets'
+
+
+
+
+
+
