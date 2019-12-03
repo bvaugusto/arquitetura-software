caf53d4151ba90012d2921126d322f44c2094b1b
Bruno <bvaugusto@gmail.com>
2019-12-03 09:38:51 -0300

    Projeto inicial Arquitetura para Aplicações Móveis

---
+++ arquitetura-aplicacoes-moveis/projeto-pwa/service_worker.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const CACHE_NAME = 'carros-cache-v1';
+
+const FILES_TO_CACHE = [
+    '/icons/128.png',
+    '/icons/144.png',
+    '/icons/152.png',
+    '/icons/180.png',
+    '/icons/256.png',
+    '/icons/512.png',    
+    '/js/bootstrap.min.js',
+    '/js/init.js',
+    '/js/jquery-3.3.1.slim.min.js',
+    '/js/popper.min.js',
+    '/css/bootstrap.min.css',
+    '/css/styles.css',
+    '/offline.html',
+    '/manifest.json',
+    '/img/offline.png',
+    '/img/logo_carro.png'
+];
+
+//Gravando arquivos arquivos estáticos - Somente para montar Front-end
+
+self.addEventListener('install', evt =>{
+
+    console.log("[Service Worker] Instalando caches estáticos");
+
+    
+    evt.waitUntil(
+
+
+        caches.open(CACHE_NAME).then((cache) => {
+            
+            return cache.addAll(FILES_TO_CACHE);
+
+        })
+        
+    );
+
+    self.skipWaiting();
+
+});
+
+//Ativando o Service Worker e removendo cache antigo
+
+
+self.addEventListener('activate', (evt) => {
+
+    console.log("[Service Worker] Ativando e removendo cache antigo");
+
+    evt.waitUntil(
+
+        caches.keys().then((keyList) => {
+
+            return Promise.all(keyList.map((key) =>{
+
+                if(key !== CACHE_NAME){
+
+                    return caches.delete(key);
+    
+                }
+
+            }));
+
+        })        
+    );  
+    self.clients.claim();
+});
+
+//Respondendo a página offline
+
+self.addEventListener('fetch', function(event) {
+
+    event.respondWith(
+      caches.match(event.request).then(function(resp) {
+        return resp || fetch(event.request).then(function(response) {
+          caches.open(CACHE_NAME).then(function(cache) {
+            //cache.put(event.request, response.clone());
+          });
+          return response;
+        });
+      }).catch(function() {
+        return caches.match('/offline.html');
+      })
+    );
+  });
+
---
+++ arquitetura-front-end/otimizacao-sites/site-otimizado/src/assets/index.js
@@ -0,0 +1,5 @@
+import './css/style.css'
+import './css/A.style-8.7_d91.css'
+import './css/A.styles-4.7.css'
+// import './scss/index.scss'
+import './js/*.*'
