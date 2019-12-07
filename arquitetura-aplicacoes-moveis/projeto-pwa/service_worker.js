ab102b75e158b20519f0320aec8ba30ccc473c15
Bruno <bvaugusto@gmail.com>
2019-12-07 19:11:30 -0300

    Refatoração do código

--- arquitetura-aplicacoes-moveis/projeto-pwa/service_worker.js
+++ arquitetura-aplicacoes-moveis/projeto-pwa/service_worker.js
@@ -1,88 +1,100 @@
-'use strict';
+(() => {
+    'use strict';
 
-const CACHE_NAME = 'projeto-pwa-v1';
+    const CACHE_NAME = 'projeto-pwa-v1';
+    const CACHE_DINAMIC = 'projeto-pwa';
 
-const FILES_TO_CACHE = [
-    '/icons/128.png',
-    '/icons/144.png',
-    '/icons/152.png',
-    '/icons/180.png',
-    '/icons/256.png',
-    '/icons/512.png',    
-    '/js/bootstrap.min.js',
-    '/js/init.js',
-    '/js/jquery-3.3.1.slim.min.js',
-    '/js/popper.min.js',
-    '/css/bootstrap.min.css',
-    '/css/styles.css',
-    '/offline.html',
-    '/manifest.json',
-    '/img/offline.png'
-];
+    const FILES_TO_CACHE = [
+        '/src/public/js/jquery-3.3.1.slim.min.js',
+        '/src/public/js/popper.min.js',
+        '/src/public/js/bootstrap.min.js',
+        '/src/public/js/init.js',
+        '/src/public/css/bootstrap.min.css',
+        '/src/public/css/main.css',
+        '/src/public/css/bootstrap.min.css',
+        '/src/public/css/main.css',
+        '/src/public/img/logo/logo.png',
+        '/src/public/img/produtos/german-weizen/1.jpg',
+        '/src/public/img/produtos/diabolique/1.png',
+        '/src/public/img/produtos/blondine/1.png',
+        '/src/public/img/produtos/wals/1.png',
+        '/src/public/img/produtos/andarilha/1.jpg',
+        '/src/public/img/produtos/brown/1.jpg',
+        '/src/public/img/produtos/conexao/1.jpeg',
+        '/src/public/img/produtos/chimay/1.png',
+        '/manifest.json',
+        '/src/public/img/icons/favicon-32x32.png',
+        '/src/public/img/icons/android-icon-144x144.png',
+        '/src/public/img/icons/favicon-32x32.png',
+        '/src/public/img/icons/android-icon-144x144.png',
+        '/service_worker.js',
+    ];
 
 //Gravando arquivos arquivos estáticos - Somente para montar Front-end
 
-self.addEventListener('install', evt =>{
+    self.addEventListener('install', evt => {
+        console.log('======= entrou addEventListener install ======');
 
-    console.log("[Service Worker] Instalando caches estáticos");
-
-    
-    evt.waitUntil(
+        console.log("[Service Worker] Instalando caches estáticos");
 
 
-        caches.open(CACHE_NAME).then((cache) => {
-            
-            return cache.addAll(FILES_TO_CACHE);
+        evt.waitUntil(
+            caches.open(CACHE_NAME).then((cache) => {
 
-        })
-        
-    );
+                return cache.addAll(FILES_TO_CACHE);
 
-    self.skipWaiting();
+            })
+        );
 
-});
+        self.skipWaiting();
+
+    });
 
 //Ativando o Service Worker e removendo cache antigo
 
 
-self.addEventListener('activate', (evt) => {
+    self.addEventListener('activate', (evt) => {
+        console.log('======= entrou addEventListener activate ======');
 
-    console.log("[Service Worker] Ativando e removendo cache antigo");
+        console.log("[Service Worker] Ativando e removendo cache antigo");
 
-    evt.waitUntil(
+        evt.waitUntil(
+            caches.keys().then((keyList) => {
 
-        caches.keys().then((keyList) => {
+                return Promise.all(keyList.map((key) => {
 
-            return Promise.all(keyList.map((key) =>{
+                    if (key !== CACHE_NAME) {
 
-                if(key !== CACHE_NAME){
+                        return caches.delete(key);
 
-                    return caches.delete(key);
-    
-                }
+                    }
 
-            }));
+                }));
 
-        })        
-    );  
-    self.clients.claim();
-});
+            })
+        );
+        self.clients.claim();
+    });
 
 //Respondendo a página offline
 
-self.addEventListener('fetch', function(event) {
+    self.addEventListener('fetch', function (event) {
 
-    event.respondWith(
-      caches.match(event.request).then(function(resp) {
-        return resp || fetch(event.request).then(function(response) {
-          caches.open(CACHE_NAME).then(function(cache) {
-            //cache.put(event.request, response.clone());
-          });
-          return response;
-        });
-      }).catch(function() {
-        return caches.match('/offline.html');
-      })
-    );
-  });
+        console.log('======= entrou addEventListener fetch ======');
 
+        event.respondWith(
+            caches.match(event.request).then(function (resp) {
+                return resp || fetch(event.request).then(function (response) {
+                    caches.open(CACHE_NAME).then(function (cache) {
+                        //cache.put(event.request, response.clone());
+                    });
+                    return response;
+                });
+            }).catch(function () {
+                return caches.match('./src/views/offline.html');
+            })
+        );
+    });
+
+
+})()
