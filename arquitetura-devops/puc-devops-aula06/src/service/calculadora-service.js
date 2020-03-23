e1b9573c4dce86306a28d173c3f2756b4a5f0d88
Bruno <bvaugusto@gmail.com>
2020-03-23 00:27:23 -0300

    update project

---
+++ arquitetura-devops/puc-devops-aula06/src/service/calculadora-service.js
@@ -0,0 +1,20 @@
+class CalculadoraService {
+
+    additionNumber(valor1, valor2) {
+        return valor1 + valor2;
+    }
+
+    subtractionNumber(valor1, valor2) {
+        return valor1 - valor2;
+    }
+
+    multiplicationNumber(valor1, valor2) {
+        return valor1 * valor2;
+    }
+
+    divisionNumber(valor1, valor2) {
+        return valor1 / valor2;
+    }
+}
+
+module.exports = CalculadoraService;
